0ecd8ca90de3b07779adaa0df90efeacf101d106
Michele Memoli <michele@100shapes.com>
2015-07-13 18:16:55 +0100

    Added use strict mode for grunt

--- src/component.js
+++ src/component.js
@@ -1,17 +1,19 @@
-'use strict';
-
-var ngModule = angular.module('climb', ['ng']);
-
-
-ngModule.factory('ClimbService', ClimbService);
-
-function ClimbService() {
-
-    var climbService = {
-
-    };
-
-    return climbService;
-}
+(function () {
+    'use strict';
 
 
+    var ngModule = angular.module('climb', ['ng']);
+
+
+    ngModule.factory('ClimbService', ClimbService);
+
+    function ClimbService() {
+
+        var climbService = {
+
+        };
+
+        return climbService;
+    }
+
+}());
