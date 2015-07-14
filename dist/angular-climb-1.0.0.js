6c746ce6ee49dc988a724a201b65a649d3ae37a1
Michele Memoli <michele@100shapes.com>
2015-07-14 09:38:36 +0100

    Build module with UMD wrapper

--- dist/angular-climb-1.0.0.js
+++ dist/angular-climb-1.0.0.js
@@ -1,6 +1,22 @@
-(function () {
+(function (root, factory) {
   'use strict';
-  var ngModule = angular.module('climb', ['ng']);
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define(['angular'], factory);
+  } else if (typeof module !== 'undefined' && typeof module.exports === 'object') {
+    // CommonJS support (for us webpack/browserify/ComponentJS folks)
+    module.exports = factory(require('angular'));
+  } else {
+    // in the case of no module loading system
+    // then don't worry about creating a global
+    // variable like you would in normal UMD.
+    // It's not really helpful... Just call your factory
+    return factory(root.angular);
+  }
+}(this, function (angular) {
+  'use strict';
+  var moduleName = 'climb';
+  var ngModule = angular.module(moduleName, ['ng']);
   ngModule.constant('CLIMB_BASE_URL', 'http://app.climb.social/api/collections/').factory('ClimbFactory', [
     '$http',
     'CLIMB_BASE_URL',
@@ -22,4 +38,5 @@
       }
     };
   }
-}());
+  return moduleName;
+}));
